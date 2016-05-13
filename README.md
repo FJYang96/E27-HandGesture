fa9ccee504768240a8f6bb85425aa37b9364ad54
carbondioxide <yangfengjun96@gmail.com>
2016-05-12 22:58:12 -0400

    trying to find palm

---
+++ README.md
@@ -0,0 +1,6 @@
+Assume hand will be the largest contour in the frame
+Prefer clean background (little variance in terms of color and brightness)
+
+Usage of file:
+	python gesture.py [filename]
+	
--- gesture.py
+++ gesture.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from numpy.linalg import det
+from math import sqrt
 from sys import argv
 
 def extract(image, mode="Adaptive"):
@@ -37,15 +39,73 @@ def findLargestContour(image):
             ci = i
     return contours[ci]
 
-def displayContour(contour, image, drawOn=False,color=[0,255,0],filled=-1):
-    ## Draw Contours
-    display = np.zeros((image.shape[0],image.shape[1],3),dtype='uint8')
-    cv2.drawContours( display, [contour], 0, color, filled)
-    if not drawOn:
-        return display
+def displayInfo(image, contour, hull):
+    """
+    Draw the contour and hull of the contour on the image
+    Here hull is the 1D indices matrix
+    """
+    #display = np.zeros((image.shape[0],image.shape[1],3),dtype='uint8')
+    img = image.copy()
+    cv2.drawContours( img, [contour], 0, [0,255,0], -1)
+    hull_points = np.array( [contour[i][0] for i in hull] )
+    cv2.drawContours( img, [hull_points], 0, [255,0,0], 3)
+    return img
+
+def findCenter(points, center_estimate=None):
+    """
+    Given a set of defects, find the approximate center of plam
+    first find average
+    then iteratively find the center of the circle that passes through
+    the three closest points to current center
+    """
+    # First find the average
+    cent = np.zeros( (2), float)
+    if center_estimate is not None:
+        cent = center_estimate
     else:
-        cv2.drawContours( image, [contour], 0, color, filled)
-        return display
+        for point in points:
+            cent += point
+        cent = cent / len(points)
+
+    if len(points) < 3:
+        return (int(cent[0]), int(cent[1]))
+
+    for i in range(3):
+        # Find the three closest points
+        sorted(points,\
+                key=lambda p: sqrt((p[0]-cent[0])**2 + (p[1]-cent[1])**2))
+        # Find the center of circle, update cent
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        x3, y3 = points[2]
+        # Hard coded equation
+        cent[0] = det(np.array([x1**2+y1**2,y1,1],[x2**2+y2**2,y2,1],[x3**2+y3**2,y3,1])) / float(2*det(np.array([x1,y1,1],[x2,y2,1],[x3,y3,1]))) 
+        cent[1] = det(np.array([x1,x1**2+y1**2,1],[x2,x2**2+y2**2,1],[x3,x3**2+y3**2,1])) / float(2*det(np.array([x1,y1,1],[x2,y2,1],[x3,y3,1]))) 
+
+    return (int(cent[0]), int(cent[1]))
+    
+def handAnalysis(image, contour, hull):
+    """
+    Analyze the gesture of the hand in the image using contour and hull
+    """
+    # Find defect points
+    defects = cv2.convexityDefects(contour,hull)
+    # Unpack the points in defects and find the average defect
+    defs = []
+    for defect in defects:
+        # start and end on the hull, far is the valley
+        start = tuple(contour[defect[0,0]][0])
+        end = tuple(contour[defect[0,1]][0])
+        far = tuple(contour[defect[0,2]][0])
+        depth = defect[0,3]
+        defs.append(far)
+    # Find the center of the palm
+    print defs
+    cent = findCenter(defs)
+    cv2.circle(image, cent, 5, (255,255,0), 2)
+    # Iteratively find the three closest points in def to current center
+     
+
 
 #def tempAve(bg, frame):
 #    """Update bg, return a masked frame with only the foreground"""
@@ -74,19 +134,19 @@ elif len(argv) == 1:
 else:
     print "Running on input image"
 
-# Read Image
-image = cv2.imread('./Marcel-Train/Five/Five-train001.ppm', \
-        cv2.IMREAD_GRAYSCALE)
-img = extract(image)
-contour = findLargestContour(img)
-hull = cv2.convexHull(contour,returnPoints=False)
-print hull
-defects = cv2.convexityDefects(contour,hull)
-print defects
-dis = displayContour(hull, image, drawOn=True)
-# Show Original Image
-cv2.imshow('win',image)
-while cv2.waitKey() < 0 : pass
+if not camera:
+    # Read Image
+    image = cv2.imread('./Marcel-Train/Five/Five-train001.ppm', \
+            cv2.IMREAD_GRAYSCALE)
+    img = extract(image)
+    contour = findLargestContour(img)
+    # Find the indices of points in the contour array that are on the hull
+    hull = cv2.convexHull(contour,returnPoints=False)
+    display = displayInfo(image, contour, hull)
+    handAnalysis(display, contour, hull)
+    # Show Original Image
+    cv2.imshow('win',display)
+    while cv2.waitKey() < 0 : pass
 
 if camera:
     cap = cv2.VideoCapture(0)
@@ -95,13 +155,17 @@ if camera:
 
     while( cap.isOpened() ):
         ret, image = cap.read()
+        # Make a copy of the frame
         img = image.copy()
+        # Background subtraction
         fg_mask = fgbg.apply(image)
+        # Find the largest contour, if cannot find any skip this frame
         contour = findLargestContour(fg_mask)
         if contour is None:
             continue
-        display = displayContour(contour,img,drawOn=True)
-        hull = cv2.convexHull(contour)
-        display = displayContour(hull,img,True,[255,0,0],1)
-        cv2.imshow('video',img)
+        # Find the indices of points on the contour that are on the hull
+        hull = cv2.convexHull(contour, returnPoints=False)
+        display = displayInfo(img, contour, hull)
+        handAnalysis(display, contour, hull)
+        cv2.imshow('video',display)
         cv2.waitKey(5)
